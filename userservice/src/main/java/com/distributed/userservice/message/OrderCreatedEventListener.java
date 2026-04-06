615fcc12d0e91d9118a94181c66773d63473dd42
ohj1hyeon <ojh010911@gmail.com>
2026-04-06 15:58:12 +0900

    feat: userservice 주문 이벤트 저장 및 조회 API 추가

--- userservice/src/main/java/com/distributed/userservice/message/OrderCreatedEventListener.java
+++ userservice/src/main/java/com/distributed/userservice/message/OrderCreatedEventListener.java
@@ -1,6 +1,7 @@
 package com.distributed.userservice.message;
 
 import com.distributed.userservice.dto.OrderCreatedEvent;
+import com.distributed.userservice.service.OrderEventHistoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -9,8 +10,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderCreatedEventListener {
 
+    private final OrderEventHistoryService orderEventHistoryService;
+
+    public OrderCreatedEventListener(OrderEventHistoryService orderEventHistoryService) {
+        this.orderEventHistoryService = orderEventHistoryService;
+    }
+
     @RabbitListener(queues = "${app.rabbitmq.order-created.queue}")
     public void handle(OrderCreatedEvent event) {
+        orderEventHistoryService.saveOrderCreatedEvent(event);
         log.info("RabbitMQ 주문 생성 이벤트 수신 완료. orderId={}, userId={}, productId={}",
                 event.getOrderId(),
                 event.getUserId(),
